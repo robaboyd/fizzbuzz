4f81d10d7032360bce25a2772552ba24631a9874
Bobby Boyd <bboyd2008@gmail.com>
2018-09-20 01:19:36 -0700

    added console logs for fizz and buzz

--- main.js
+++ main.js
@@ -6,7 +6,12 @@
 fizzBuzz();
 
 function fizzBuzz() {
-  for (let i = 1; i < 100; i++) {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+      console.log(i + " fizz");
+    } else if (i % 5 === 0) {
+      console.log(i + " buzz");
+    }
     console.log(i);
   }
 }
