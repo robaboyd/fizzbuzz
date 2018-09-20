b2b869b8ecb962918394b96e0b92df6ff9e1e03e
Bobby Boyd <bboyd2008@gmail.com>
2018-09-20 01:23:04 -0700

    completed fizzbuzz

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ fizzBuzz();
 
 function fizzBuzz() {
   for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-      console.log(i + " fizz");
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i + " fizzbuzz");
     } else if (i % 5 === 0) {
       console.log(i + " buzz");
+    } else if (i % 3 === 0) {
+      console.log(i + " fizz");
     }
     console.log(i);
   }
