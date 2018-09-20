3e52f7440580fb748a1704300ef4f75a21935d51
Bobby Boyd <bboyd2008@gmail.com>
2018-09-20 01:34:50 -0700

    better with a variable

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ fizzBuzz();
 
 function fizzBuzz() {
   for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log(i + " fizzbuzz");
-    } else if (i % 5 === 0) {
-      console.log(i + " buzz");
-    } else if (i % 3 === 0) {
-      console.log(i + " fizz");
+    let word = " ";
+    if (i % 3 === 0) {
+      word += "fizz";
     }
-    console.log(i);
+    if (i % 5 === 0) {
+      word += "buzz";
+    }
+    console.log(i + word);
   }
 }
